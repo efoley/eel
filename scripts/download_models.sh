0552023e5d5958168154f9bee9d0bf9751a17de1
Eric Foley <ericdfoley@gmail.com>
2025-03-19 09:07:48 -0600

    fix: use Qwen2.5-0.5B-Instruct as default

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -3,12 +3,19 @@ set -euxo pipefail
 
 
 ORG=Qwen
-MODEL=Qwen2.5-Coder-0.5B
+MODEL=Qwen2.5-0.5B-Instruct
 
 mkdir -p data/${MODEL}
 cd data/${MODEL}
 
+FILES=(
+    "config.json"
+    "tokenizer.json"
+    "tokenizer_config.json"
+    "model.safetensors"
+)
+
 # loop over the names config.json, tokenizer.json, and model.safetensors and download them all
-for file in config.json tokenizer.json model.safetensors; do
+for file in "${FILES[@]}"; do
     curl -L "https://huggingface.co/${ORG}/${MODEL}/resolve/main/${file}" -o $file
 done
